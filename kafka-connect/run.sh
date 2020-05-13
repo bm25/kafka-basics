93fc7363f0e2a10ee9b15590e03670c7bc81302a
anton <blunt2fakie@yandex.ru>
2020-05-13 03:12:20 -0700

    Declarative producer was added as Kafka Connect Source (just a folder with standalone settings). Can be started using run.sh

---
+++ kafka-connect/run.sh
@@ -0,0 +1,7 @@
+#!usr/bin/env bash
+#Run the twitter connector
+#connect-standalone connect-standalone.properties twitter.properties
+#OR Linux/mac OSX
+connect-standalone.sh connect-standalone.properties twitter.properties
+#OR WINDOWS
+#connect-standalone.bat connect-standalone.properties twitter.properties
